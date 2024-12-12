091c52acb9482e616f6bf6ed1feff167d6a2995f
Augustine-jr <anekeaustin@gmail.com>
2024-12-12 18:52:55 +0100

    create newsletterSubscriberModel.ts

---
+++ server/models/newsletterSubscriberModel.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const newsletterSubscriberSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    match: [/^\S+@\S+\.\S+$/, 'Please provide a valid email address']
+  },
+  subscribedAt: {
+    type: Date,
+    default: Date.now
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  }
+});
+
+
+const NewsletterSubscriber = mongoose.models.NewsletterSubscriber || 
+  mongoose.model('NewsletterSubscriber', newsletterSubscriberSchema);
+
+export default NewsletterSubscriber; 
