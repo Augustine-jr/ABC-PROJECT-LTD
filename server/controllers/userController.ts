7a165a8a3a8edbec77b5adce05ea3883e1812634
Augustine-jr <anekeaustin@gmail.com>
2024-12-12 10:54:28 +0100

    Create userControllers functions for user management and authentication

---
+++ server/controllers/userController.ts
@@ -0,0 +1,32 @@
+
+// Route for user login
+const loginUser = async (req, res) => {
+
+}
+
+// Route for user registration
+const registerUser = async (req, res) => {
+
+}
+
+// Route for user password reset request (forgot password)
+const forgotPassword = async (req, res) => {
+  // Handle forgot password logic
+};
+
+// Route for resetting user password
+const resetPassword = async (req, res) => {
+  // Handle reset password logic
+};
+
+// Route for user logout (invalidating JWT or session)
+const logoutUser = async (req, res) => {
+  // Logout logic
+};
+
+// Route for Admin login
+const adminLogin = async (req, res) => {
+
+}
+
+export { logoutUser, resetPassword, forgotPassword, adminLogin, loginUser, registerUser };
