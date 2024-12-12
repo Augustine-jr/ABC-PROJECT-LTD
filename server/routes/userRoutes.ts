65bf47b33eb33a2683463c9183aac52bd51ff35f
Augustine-jr <anekeaustin@gmail.com>
2024-12-12 11:18:22 +0100

    Create userRoutes for user operations

---
+++ server/routes/userRoutes.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import {  loginUser,  registerUser,  forgotPassword,  resetPassword,  logoutUser,  adminLogin } from '../controllers/userController.ts';
+  
+const userRouter = express.Router();
+
+// Route for user login
+userRouter.post('/login', loginUser);
+
+// Route for user registration
+userRouter.post('/register', registerUser);
+
+// Route for forgot password (sending password reset link)
+userRouter.post('/forgot-password', forgotPassword);
+
+// Route for resetting password
+userRouter.post('/reset-password', resetPassword);
+
+// Route for user logout
+userRouter.post('/logout', logoutUser);
+
+// Route for Admin login
+userRouter.post('/admin/login', adminLogin);
+
+export default userRouter;
+ 
+ 
+ 
